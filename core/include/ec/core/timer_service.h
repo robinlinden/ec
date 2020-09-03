08df123c7cfd30e47de8677fba3464e486b75f68
Robin Linden <dev@robinlinden.eu>
2020-09-03 21:54:37 +0200

    Only use 1 thread in the timer service

--- core/include/ec/core/timer_service.h
+++ core/include/ec/core/timer_service.h
@@ -5,52 +5,78 @@
 
 #include <chrono>
 #include <condition_variable>
+#include <functional>
 #include <mutex>
 #include <thread>
+#include <queue>
 
 namespace ec::core {
 
-// TODO(robinlinden): Use a queue or thread pool or something instead
-//  throwing threads around like this.
 class TimerService {
 public:
     explicit TimerService(ActiveObject *ctx) : ctx_{ctx} {}
+
     ~TimerService() {
         running_.store(false);
-        cv_.notify_all();
-        for (auto &thread : threads_) { thread.join(); }
+        cv_.notify_one();
+        thread_.join();
     }
 
     void call_after(std::chrono::milliseconds timeout, auto function) {
-        threads_.push_back(std::thread([=, this] {
-            std::unique_lock<std::mutex> lock{mutex_};
-            cv_.wait_for(lock, timeout);
-
-            if (running_.load()) {
-                ctx_->call(function);
-            }
-        }));
+        std::unique_lock<std::mutex> lock{mutex_};
+        jobs_.push(TimerJob{clock::now() + timeout, function});
+        cv_.notify_one();
     }
 
     void call_every(std::chrono::milliseconds timeout, auto function) {
-        threads_.push_back(std::thread([=, this] {
-            while (running_.load()) {
-                std::unique_lock<std::mutex> lock{mutex_};
-                cv_.wait_for(lock, timeout);
-
-                if (running_.load()) {
-                    ctx_->call(function);
-                }
-            }
-        }));
+        std::unique_lock<std::mutex> lock{mutex_};
+        jobs_.push(TimerJob{clock::now() + timeout, function, timeout});
+        cv_.notify_one();
     }
 
 private:
+    using clock = std::chrono::steady_clock;
+
+    struct TimerJob {
+        std::chrono::time_point<std::chrono::steady_clock> deadline;
+        std::function<void()> function;
+        std::chrono::milliseconds period{0};
+
+        bool operator>(const TimerJob &other) const {
+            return deadline > other.deadline;
+        }
+    };
+
+    void run() {
+        while(running_.load()) {
+            std::unique_lock<std::mutex> lock{mutex_};
+            if (jobs_.empty()) {
+                cv_.wait(lock, [&] { return !jobs_.empty() || !running_.load(); });
+                continue;
+            }
+
+            cv_.wait_until(lock, jobs_.top().deadline, [&] {
+                return jobs_.top().deadline < clock::now() || !running_.load();
+            });
+
+            if (running_.load()) {
+                auto job = jobs_.top();
+                ctx_->call(job.function);
+                jobs_.pop();
+                if (job.period.count() != 0) {
+                    job.deadline = clock::now() + job.period;
+                    jobs_.push(job);
+                }
+            }
+        }
+    }
+
     ActiveObject *const ctx_;
     std::atomic<bool> running_{true};
     std::condition_variable cv_{};
     std::mutex mutex_{};
-    std::vector<std::thread> threads_{};
+    std::priority_queue<TimerJob, std::vector<TimerJob>, std::greater<>> jobs_{};
+    std::thread thread_{&TimerService::run, this};
 };
 
 } // namespace ec::core
